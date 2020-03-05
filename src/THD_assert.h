3a07f85bfb05c64272b7240c673e6dd62c4dc8b3
Ludovic Capelli <l.capelli@ed.ac.uk>
2020-03-05 19:38:48 +0000

    Line added in summary. Singular / Plural integrated.

--- src/THD_assert.h
+++ src/THD_assert.h
@@ -11,16 +11,16 @@
 #include <string.h>
 #include <stdbool.h>
 
-int THD_assert_number_of_tests = 0; /* Total number of tests run */
-int THD_assert_number_of_successes = 0; /* Total number of tests that succeeded*/
-int THD_assert_my_number_of_tests = 0;
+int THD_assert_number_of_tests= 0; /* Total number of testsrun */
+int THD_assert_number_of_successes = 0; /* Total number of teststhat succeeded*/
+int THD_assert_my_number_of_tests= 0;
 int THD_assert_my_number_of_successes = 0;
 char* THD_assert_previous_function = NULL; /* Function in which the previous assert was performed. */
 
 /**
  * @param[in] function The name of the function from which the assert is called.
  **/
-void thd_assert(bool ARGS, const char* label, const char* function)
+void THD_assert_function(bool ARGS, const char* label, const char* function, int line)
 {
 	// Check if I am first assert in entire program
 	if(THD_assert_previous_function == NULL)
@@ -37,7 +37,7 @@ void thd_assert(bool ARGS, const char* label, const char* function)
 		strncpy(THD_assert_previous_function, function, strlen(function) + 1);
 		printf("THD asserts:\n");
 		printf(" | \n");
-		printf(" ├>\x1b[;1m Function %s\x1b[0m\n", function);
+		printf(" ├>\x1b[;1m %s\x1b[0m\n", function);
 	}
 	else
 	{
@@ -50,7 +50,7 @@ void thd_assert(bool ARGS, const char* label, const char* function)
 	{
 		// I am in a different function
 		// Print the old function-local totals
-		printf("\033[2K\r |    └> \x1b[34;1m[RESULTS]\x1b[0m %d tests performed: \x1b[32;1m%d successes\x1b[0m and \x1b[31;1m%d failures\x1b[0m.\n", THD_assert_my_number_of_tests, THD_assert_my_number_of_successes, THD_assert_my_number_of_tests - THD_assert_my_number_of_successes);
+		printf("\033[2K\r |    └> \x1b[34;1m[RESULTS]\x1b[0m %d tests: \x1b[32;1m%d success%s\x1b[0m and \x1b[31;1m%d failure%s\x1b[0m.\n", THD_assert_my_number_of_tests, THD_assert_my_number_of_successes, (THD_assert_my_number_of_successes > 1) ? "es": "", THD_assert_my_number_of_tests- THD_assert_my_number_of_successes, (THD_assert_my_number_of_tests- THD_assert_my_number_of_successes > 1) ? "s" : "");
 		printf("\033[2K\r |\n");
 
 		// Store the new function name 
@@ -64,10 +64,10 @@ void thd_assert(bool ARGS, const char* label, const char* function)
 		strncpy(THD_assert_previous_function, function, strlen(function) + 1);
 
 		THD_assert_my_number_of_successes = 0;
-		THD_assert_my_number_of_tests = 0;
+		THD_assert_my_number_of_tests= 0;
 
 		// Print the new function title
-		printf("\033[2K\r ├>\x1b[;1m Function %s\x1b[0m\n", function);
+		printf("\033[2K\r ├>\x1b[;1m %s\x1b[0m\n", function);
 	}
 
 	// Do the actual assert
@@ -82,18 +82,18 @@ void thd_assert(bool ARGS, const char* label, const char* function)
 	{
 		printf("\x1b[31m[FAILED]\x1b[0m ");
 	}
-	printf("Asserting \"%s\".\n", label);
+	printf("Line %d: \"%s\".\n", line, label);
 
 	// Print the old function-local totals
 	THD_assert_my_number_of_tests++;
-	printf("\033[2K\r |    └> \x1b[34;1m[RESULTS]\x1b[0m %d tests performed: \x1b[32;1m%d successes\x1b[0m and \x1b[31;1m%d failures\x1b[0m.\n", THD_assert_my_number_of_tests, THD_assert_my_number_of_successes, THD_assert_my_number_of_tests - THD_assert_my_number_of_successes);
+	printf("\033[2K\r |    └> \x1b[34;1m[RESULTS]\x1b[0m %d tests: \x1b[32;1m%d success%s\x1b[0m and \x1b[31;1m%d failure%s\x1b[0m.\n", THD_assert_my_number_of_tests, THD_assert_my_number_of_successes, (THD_assert_my_number_of_successes > 1) ? "es": "", THD_assert_my_number_of_tests- THD_assert_my_number_of_successes, (THD_assert_my_number_of_tests- THD_assert_my_number_of_successes > 1) ? "s" : "");
 	printf("\033[2K\r |\n");
 
 	// Print the general totals in case it is the last assert
 	THD_assert_number_of_tests++;
-	printf(" └> \x1b[34;1m[RESULTS]\x1b[0m %d tests performed: \x1b[32;1m%d successes\x1b[0m and \x1b[31;1m%d failures\x1b[0m.\n", THD_assert_number_of_tests, THD_assert_number_of_successes, THD_assert_number_of_tests - THD_assert_number_of_successes);
+	printf(" └> \x1b[34;1m[SUMMARY]\x1b[0m %d tests: \x1b[32;1m%d successes\x1b[0m and \x1b[31;1m%d failures\x1b[0m.\n", THD_assert_number_of_tests, THD_assert_number_of_successes, THD_assert_number_of_tests- THD_assert_number_of_successes);
 }
 
-#define THD_assert(ARGS) thd_assert(ARGS, #ARGS, __func__);
+#define THD_assert(ARGS) THD_assert_function(ARGS, #ARGS, __func__, __LINE__);
 
 #endif
